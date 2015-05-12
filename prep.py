23d1ccfc12af536a971eefe7292719eadb2589fc
Zeerak Waseem <zeerak.w@gmail.com>
2015-05-12 23:08:04 +0200

    so far

--- prep.py
+++ prep.py
@@ -1,5 +1,11 @@
 from skimage.features import canny
 from scipy import ndimage
+from glob import glob
+import os
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import k_means
 
 """
 DONE
@@ -23,10 +29,28 @@ def get_features(img, sigma):
 
 def get_prediction(clf, train, test):
     clf.fit(train)
-    clf.predict(test)
+    return clf.predict(test)
 
 def main():
-    # TODO Get canny features for test set
-    # TODO Get canny features for training set
-
+    thumbs_h = glob(os.getcwd() + '/data/thumbs_up/human/')
+    thumbs_d = glob(os.getcwd() + '/data/thumbs_up/Drawn/')
+    peace_h  = glob(os.getcwd() + '/data/peace/human/')
+    peace_h  = glob(os.getcwd() + '/data/peace/Drawn/')
     
+    thumbs_h_feats = []
+    thumbs_d_feats = []
+    peace_h_feats  = []
+    peace_d_feats  = []
+    for i in range(len(thumbs_h)):
+        thumbs_h_feats.append(get_features(thumbs_h[i], 3))
+        peace_h_feats.append(get_features(peace_h[i], 3))
+        thumbs_d_feats.append(get_features(thumbs_d[i], 3))
+        peace_d_feats.append(get_features(peace_d[i], 3))
+        
+    classifiers = [KNeighborsClassifier(), SVC(), LogisticRegression()] 
+    
+    predictions = []
+    for clf in classifiers:
+        get_prediction(clf, thumbs_h_feats, )
+
+
