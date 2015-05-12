a47a7032c286ac0c355c80b34b23112735979e5d
Zeerak Waseem <zeerak.w@gmail.com>
2015-05-13 01:51:35 +0200

    prep nearly done

--- prep.py
+++ prep.py
@@ -1,22 +1,26 @@
-from skimage.feature import canny
-from scipy import ndimage
-from glob import glob
-import numpy as np
 import os
+import random
+import numpy as np
+from PIL import Image
+from glob import glob
+from scipy import ndimage
 from sklearn.svm import SVC
+from skimage.feature import canny
+from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.cluster import k_means
-from PIL import Image
+from sklearn.cross_validation import StratifiedKFold
 
 """
 DONE
+0. Resize all images to same size
 1. Extract canny features from training set (either human OR drawn) 
 2. Add labels to data
+3. Add cross validation
+
 
 TODO
-0. Resize all images to same size
-1. 
+1. Shuffle data
 2. Feed features to classifier(s) - Classifiers: KNearest, SVM, LogisticRegression, k_means
 3. Predict on the test set
 """
@@ -31,16 +35,28 @@ def get_features(img, sigma):
 
     return canny(img, sigma)
 
-def get_prediction(clf, train, test):
-    # TODO Crossvalidate here
-    clf.fit(train)
-    return clf.predict(test)
+def get_prediction(clf, trainX, trainY, testX, testY):
+    
+    accuracies = {}
+    fold_acc = []
+    for train, test in StratifiedKFold(labels, n_folds = 4):
+        clf.fit(trainX[train])
+        pred = clf.predict(trainX[test])
+        fold_acc.append(accuracy_score(trainY[test], pred))
+    
+    pred = clf.predict(testX)
+
+    accuracies['folds'] = fold_acc
+    accuracies['testSet'] = accuracy_score(testY, pred)
+
+    return accuracies
 
 def main():
-    thumbs_h = glob(os.getcwd() + '/data/thumbs_up/human/*.jpg')
-    thumbs_d = glob(os.getcwd() + '/data/thumbs_up/Drawn/*.jpg')
-    peace_h  = glob(os.getcwd() + '/data/peace/human/*.jpg')
-    peace_d  = glob(os.getcwd() + '/data/peace/Drawn/*.jpg')
+    # Get filenames
+    thumbs_h = glob(os.getcwd() + '/data/thumbs_up/human/*')
+    thumbs_d = glob(os.getcwd() + '/data/thumbs_up/Drawn/*')
+    peace_h  = glob(os.getcwd() + '/data/peace/human/*')
+    peace_d  = glob(os.getcwd() + '/data/peace/Drawn/*')
 
     """
     Data structure:
@@ -49,32 +65,34 @@ def main():
     print humans[i][0] accesses image
     print humans[i][1] accesses label
     """
+
+    # Read files in and save them in np.array
     humans_t = np.array([[np.array(Image.open(thumbs).convert('L'), 'f'), 1] for thumbs in thumbs_h])
-    humans_p = np.array([[np.array(Image.open(peace).convert('L'), 'f'), 1] for peace in peace_h])
-    drawn_t  = np.array([[np.array(Image.open(thumbs).convert('L'), 'f'), 0] for thumbs in thumbs_d])
+    humans_p = np.array([[np.array(Image.open(peace).convert('L'), 'f'), 0] for peace in peace_h])
+    drawn_t  = np.array([[np.array(Image.open(thumbs).convert('L'), 'f'), 1] for thumbs in thumbs_d])
     drawn_p  = np.array([[np.array(Image.open(peace).convert('L'), 'f'), 0] for peace in peace_d])
     
     # Stack all human and drawn images
     humans = np.vstack((humans_t, humans_p))
     drawings = np.vstack((drawn_t, drawn_p))
-
+    
     # Get features and labels 
     h_feats = []
     h_labels = []
-
     d_feats = []
     d_labels = []
 
-    for i in range(40):
-        h_feats.append(get_features(humans[i][0]), sigma=3)
+    for i in range(len(humans)):
+        h_feats.append(get_features(humans[i][0], sigma=3))
         h_labels.append(humans[i][1])
-        d_feats.append(get_features(drawings[i][0]), sigma=3)
+        d_feats.append(get_features(drawings[i][0], sigma=3))
         d_labels.append(drawings[i][1])
 
-    # classifiers = [KNeighborsClassifier(), SVC(), LogisticRegression()] 
+    classifiers = [KNeighborsClassifier(), SVC(), LogisticRegression()] 
     
-    # predictions = []
-    # for clf in classifiers:
-    #     get_prediction(clf, thumbs_h_feats, )
+    pred = {}
+    for clf in classifiers:
+        pred[clf] = get_prediction(clf, h_feats, h_labels)
+
 
 main()
